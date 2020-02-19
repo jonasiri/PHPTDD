7791fa24b3f45e35cb0f4791e0dcf03fe0fb8868
Jona <jsiri@sherwoodsys.com>
2020-02-19 13:21:48 +0000

    added new book path

--- app/Book.php
+++ app/Book.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Book extends Model
 {
-    protected $guarded = []; 
+    protected $guarded = [];
+
+    public function path() {
+        return route('get-book', $this->id);
+    }
 }
