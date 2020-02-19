3da79a66fe8c2aeb3e8bae3df05d816c8e5cc308
Jona <jsiri@sherwoodsys.com>
2020-02-19 10:39:05 +0000

    add basic tests for Book

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -3,19 +3,74 @@
 namespace Tests\Feature;
 
 use Tests\TestCase;
+use App\Book;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
-class ExampleTest extends TestCase
+class BookReservationTest extends TestCase
 {
-    /**
-     * A basic test example.
-     *
-     * @return void
-     */
-    public function testBasicTest()
-    {
-        $response = $this->get('/');
+    use RefreshDatabase;
 
-        $response->assertStatus(200);
+     /** @test */
+    public function test_a_book_can_be_added_to_the_library()
+    {
+        $this->withoutExceptionHandling();
+
+        $response = $this->post('/books', [
+            'title'=>'title',
+            'author'=> 'Victor'
+        ]);
+
+        $response->assertOk();
+        $this->assertCount(1, Book::all());
+
     }
+
+     /** @test */
+    public function a_title_is_required() {
+
+        $response = $this->post('/books', [
+            'title'=>'',
+            'author'=> 'Victor'
+        ]);
+
+        $response->assertSessionHasErrors('title');
+
+    }
+
+     /** @test */
+     public function an_author_is_required() {
+
+        $response = $this->post('/books', [
+            'title'=>'cool',
+            'author'=> ''
+        ]);
+
+        $response->assertSessionHasErrors('author');
+
+    }
+
+      /** @test */
+      public function a_book_can_be_updated() {
+
+        $this->withoutExceptionHandling();
+
+         $this->post('/books', [
+            'title'=>'cool',
+            'author'=> 'steve'
+        ]);
+
+        $book = Book::first();
+
+        $response = $this->patch('/books/' . $book->id,[
+            'title'=>'smith',
+            'author'=> 'chris'
+        ]);
+
+        $this->assertEquals('smith', Book::first()->title);
+        $this->assertEquals('chris', Book::first()->author);
+
+
+    }
+
+
 }
